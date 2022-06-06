a9786cd2ef33eb51d2197fe1e6d42fe59c0b1b35
cijang74 <82044367+cijang74@users.noreply.github.com>
2022-06-06 11:33:11 +0900

    시험공부_자구
    트리 배열 표현법

--- source.c
+++ source.c
@@ -1,27 +1,53 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
-int factorial(int num)
-// 팩토리얼 계산
+#define SIZE 10
+
+typedef struct node
 {
-	if (num <= 1)
+	int data;
+}node;
+
+node n1 = { 1 };
+node n2 = { 2 };
+node n3 = { 3 };
+node n4 = { 4 };
+node n5 = { 5 };
+node n6 = { 6 };
+
+void search(node* array)
+{
+	int i = 1;
+	int choose = 0;
+
+	while (i <= 6)
 	{
-		return 1;
+		printf("현재 노드의 값은 %d\n", array[i].data);
+		printf("왼쪽 자식 노드로 이동: 1 / 오른쪽 자식 노드로 이동: 2\n");
+		scanf("%d", &choose);
+
+		if (choose == 1)
+			i *= 2;
+
+		if (choose == 2)
+			i = i * 2 + 1;
 	}
 
-	else
-	{
-		return (num * factorial(num - 1));
-	}
+	printf("더이상 자식 노드가 존재하지 않습니다.");
 }
 
 int main()
 {
-	int num = 0;
-	int result = 0;
+	node array[SIZE];
 
-	scanf("%d", &num);
-	result = factorial(num);
+	array[1] = n1;
+	array[2] = n2;
+	array[3] = n3;
+	array[4] = n4;
+	array[5] = n5;
+	array[6] = n6;
 
-	printf("%d", result);
+	search(array);
+
 	return 0;
 }
