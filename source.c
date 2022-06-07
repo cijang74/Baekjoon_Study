a4c9ac7f0f6965328e102667b9722285ced7139f
cijang74 <82044367+cijang74@users.noreply.github.com>
2022-06-08 07:29:40 +0900

    시험공부_자구
    디렉토리 용량 계산

--- source.c
+++ source.c
@@ -9,76 +9,52 @@ typedef struct Node
 
 int exculate(Node* node)
 {
-	if (node->data == NULL)
-	{
-		return 0;
-	}
-
-	if (node->left == NULL && node->right == NULL) // 비단말 노드라면 피연산자이므로 노트의 데이터를 반환
-	{
-		return node->data;
-	}
-
-	else
+	if (node->left != NULL && node->right != NULL)
 	{
 		int op1 = exculate(node->left);
 		int op2 = exculate(node->right);
+		int result = op1 + op2;
 
-		switch (node->data)
-		{
-		case '+':
-			printf("%d + %d = %d\n", op1, op2, op1 + op2);
-			return op1 + op2;
-
-		case '*':
-			printf("%d * %d = %d\n", op1, op2, op1 * op2);
-			return op1 * op2;
-		}
+		return (node->data + result);
 	}
-
+		
+	else
+	{
+		return (node->data);
+	}
 }
 
 int main()
 {
-	Node* n1, * n2, * n3, * n4, * n5, * n6, *n7;
+	Node* n1, * n2, * n3, * n4, * n5;
 	n1 = (Node*)malloc(sizeof(Node));
 	n2 = (Node*)malloc(sizeof(Node));
 	n3 = (Node*)malloc(sizeof(Node));
 	n4 = (Node*)malloc(sizeof(Node));
 	n5 = (Node*)malloc(sizeof(Node));
-	n6 = (Node*)malloc(sizeof(Node));
-	n7 = (Node*)malloc(sizeof(Node));
 
-	n1->data = '+';
+	n1->data = 0;
 	n1->left = n2;
 	n1->right = n3;
 
-	n2->data = '*';
-	n2->left = n4;
-	n2->right = n5;
+	n2->data = 50;
+	n2->left = NULL;
+	n2->right = NULL;
 
-	n3->data = '+';
-	n3->left = n6;
-	n3->right = n7;
+	n3->data = 100;
+	n3->left = n4;
+	n3->right = n5;
 
-	n4->data = 3;
+	n4->data = 200;
 	n4->left = NULL;
 	n4->right = NULL;
 
-	n5->data = 2;
+	n5->data = 500;
 	n5->left = NULL;
 	n5->right = NULL;
 
-	n6->data = 5;
-	n6->left = NULL;
-	n6->right = NULL;
-
-	n7->data = 6;
-	n7->left = NULL;
-	n7->right = NULL;
-
 	printf("계산 결과는 %d", exculate(n1));
 
-	free(n1, n2, n3, n4, n5, n6, n7);
+	free(n1, n2, n3, n4, n5);
 	return 0;
 }
